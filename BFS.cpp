ac0e57a7fbd2f1a30413dd39e822c21a26fe85a9
originEdu <originpark82@gmail.com>
2026-06-24 11:33:10 +0900

    한번만 순회하는 BFS 코드 추가

--- BFS.cpp
+++ BFS.cpp
@@ -1,6 +1,55 @@
 ﻿#include <iostream>
+#include <queue>
+
+//BFS
+//너비 우선 탐색
+//핵심 = 다음 방문할 정점을 미리 기억해 놓기
+//기억한 정점을 먼저 기억한 순서대로 방문 -> 선입선출
+
+using namespace std;
+
+const int VertexCount = 6;
+
+int Graph[VertexCount][VertexCount] =
+{
+	//0 1 2 3 4 5
+	 {0,1,0,1,0,0},
+	 {1,0,1,1,0,0},
+	 {0,1,0,0,0,0},
+	 {1,1,0,0,1,0},
+	 {0,0,0,1,0,1},
+	 {0,0,0,0,1,0}
+};
+
+bool Visited[VertexCount] = { 0 };
+
+void BFS(int Start)
+{
+	queue<int> VertexQueue;
+	VertexQueue.push(Start);
+	Visited[Start] = true;
+
+	while (!VertexQueue.empty())
+	{
+		int Current = VertexQueue.front();
+		cout << "방문 :" << Current << endl;
+		VertexQueue.pop();
+
+		for (int Next = 0; Next < VertexCount; Next++)
+		{
+			if (Graph[Current][Next] == 0)
+				continue;
+			if (Visited[Next] == true)
+				continue;
+
+			VertexQueue.push(Next);
+			Visited[Next] = true;
+		}
+	}
+}
 
 int main()
 {
+	BFS(0);
 	return 0;
 }
