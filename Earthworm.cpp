884ec106f4459e33dd818392b01d048327edb2df
originEdu <originpark82@gmail.com>
2026-06-24 10:59:48 +0900

    DFS 안쓴 코드 최종

--- Earthworm.cpp
+++ Earthworm.cpp
@@ -1,4 +1,81 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//
+//using namespace std;
+//
+//int dir[4][2] = {
+//	//X  Y
+//	{-1, 0}, //Left
+//	{ 1, 0}, //Right
+//	{ 0, 1}, //Down
+//	{ 0,-1}  //Up
+//};
+//
+//
+//const int MaxSize = 50;
+//
+//bool Visited[MaxSize][MaxSize] = {0};
+//
+//int Map[MaxSize][MaxSize] = {0};
+//
+//int M = 0; //가로
+//int N = 0; //세로
+//
+//void DFS(int CurrentX, int CurrentY)
+//{
+//	Visited[CurrentX][CurrentY] = true;
+//	for (int l = 0; l < 4; l++)
+//	{
+//		int X = CurrentX + dir[l][0];
+//		int Y = CurrentY + dir[l][1];
+//		if (X < 0 || X >= M || Y < 0 || Y >= N)
+//		{
+//			continue;
+//		}
+//
+//		if (Map[X][Y] == 1 && (Visited[X][Y] == false))
+//		{
+//			Visited[X][Y] = true;
+//			DFS(X,Y);
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int K = 0; //배추가 심어져있는 위치
+//	int CabbageX = 0; //배추 X좌표
+//	int CabbageY = 0; //배추 Y좌표
+//	int EarthwormCount = 0; //지렁이 수
+//	scanf_s("%d %d %d", &M,&N,&K);
+//	for (int i=0;i<K;i++)
+//	{
+//		scanf_s("%d %d",&CabbageX,&CabbageY);
+//		Map[CabbageX][CabbageY] = 1;
+//	}
+//
+//	int CurrentX = 0; //현재 X위치
+//	int CurrentY = 0; //현재 Y위치
+//
+//	int X = 0; //탐색을 위한 X값
+//	int Y = 0; //탐색을 위한 Y값
+//
+//	for (int i = 0; i < M; i++)
+//	{
+//		for (int j = 0; j < N; j++)
+//		{
+//			if (Map[i][j] == 1 && (Visited[i][j]==false)) {
+//				EarthwormCount++; 
+//				DFS(i, j);        
+//			}
+//		}
+//	}
+//	
+//	cout << EarthwormCount << endl;
+//
+//	return 0;
+//}
+
+#include <iostream>
 
 using namespace std;
 
@@ -10,46 +87,24 @@ int dir[4][2] = {
 	{ 0,-1}  //Up
 };
 
-
 const int MaxSize = 50;
 
-bool Visited[MaxSize][MaxSize] = {0};
+bool Visited[MaxSize][MaxSize] = { 0 };
 
-int Map[MaxSize][MaxSize] = {0};
-
-int M = 0; //가로
-int N = 0; //세로
-
-void DFS(int CurrentX, int CurrentY)
-{
-	Visited[CurrentX][CurrentY] = true;
-	for (int l = 0; l < 4; l++)
-	{
-		int X = CurrentX + dir[l][0];
-		int Y = CurrentY + dir[l][1];
-		if (X < 0 || X >= M || Y < 0 || Y >= N)
-		{
-			continue;
-		}
-
-		if (Map[X][Y] == 1 && (Visited[X][Y] == false))
-		{
-			Visited[X][Y] = true;
-			DFS(X,Y);
-		}
-	}
-}
+int Map[MaxSize][MaxSize] = { 0 };
 
 int main()
 {
+	int M = 0; //가로
+	int N = 0; //세로
 	int K = 0; //배추가 심어져있는 위치
 	int CabbageX = 0; //배추 X좌표
 	int CabbageY = 0; //배추 Y좌표
 	int EarthwormCount = 0; //지렁이 수
-	scanf_s("%d %d %d", &M,&N,&K);
-	for (int i=0;i<K;i++)
+	scanf_s("%d %d %d", &M, &N, &K);
+	for (int i = 0; i < K; i++)
 	{
-		scanf_s("%d %d",&CabbageX,&CabbageY);
+		scanf_s("%d %d", &CabbageX, &CabbageY);
 		Map[CabbageX][CabbageY] = 1;
 	}
 
@@ -59,17 +114,63 @@ int main()
 	int X = 0; //탐색을 위한 X값
 	int Y = 0; //탐색을 위한 Y값
 
+	bool IsUpdateFlag = false; //지렁이 수 증가를 위한 플래그
+	bool 중첩플래그 = false;
+
 	for (int i = 0; i < M; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			if (Map[i][j] == 1 && (Visited[i][j]==false)) {
-				EarthwormCount++; 
-				DFS(i, j);        
+			IsUpdateFlag = false;
+			중첩플래그 = false;
+			CurrentX = i;
+			CurrentY = j;
+			if (Map[CurrentX][CurrentY] == 0)
+			{
+				continue;
+			}
+			Visited[CurrentX][CurrentY] = true;
+			IsUpdateFlag = true;
+			for (int l = 0; l < 4; l++)
+			{
+				X = CurrentX + dir[l][0];
+				Y = CurrentY + dir[l][1];
+				if (X < 0 || X >= M || Y < 0 || Y >= N)
+				{
+					continue;
+				}
+
+				if (Map[X][Y] == 0)
+				{
+					continue;
+				}
+
+				if (Visited[X][Y] == true)
+				{
+					중첩플래그 = true;
+					continue;
+				}
+
+				if (Map[X][Y] == 1)
+				{
+					Visited[X][Y] = true;
+					IsUpdateFlag = true;
+				}
+			}
+
+			if (중첩플래그)
+			{
+				IsUpdateFlag = false;
+			}
+
+			if (IsUpdateFlag)
+			{
+				cout << "X 좌표 :" << CurrentX << ", " << "Y 좌표 : " << CurrentY << endl;
+				EarthwormCount++;
 			}
 		}
 	}
-	
+
 	cout << EarthwormCount << endl;
 
 	return 0;
