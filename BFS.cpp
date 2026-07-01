939a6347da9d17aa6001ab8adef1460f0a234711
originEdu <originpark82@gmail.com>
2026-07-01 16:05:25 +0900

    Tree.cpp 추가

--- BFS.cpp
+++ BFS.cpp
@@ -1,114 +1,114 @@
-﻿#include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
-
-//BFS
-//너비 우선 탐색
-//핵심 = 다음 방문할 정점을 미리 기억해 놓기
-//기억한 정점을 먼저 기억한 순서대로 방문 -> 선입선출
-
-using namespace std;
-
-const int VertexCount = 6;
-
-int Graph[VertexCount][VertexCount] =
-{
-	//0 1 2 3 4 5
-	 {0,1,0,1,0,0},
-	 {1,0,1,1,0,0},
-	 {0,1,0,0,0,0},
-	 {1,1,0,0,1,0},
-	 {0,0,0,1,0,1},
-	 {0,0,0,0,1,0}
-};
-
-bool Visited[VertexCount] = { 0 };
-
-int Distance[VertexCount] = { 0 }; // 시작 지점으로 부터 떨어진 거리 기록
-int Parent[VertexCount] = { 0 };   // 해당 정점의 부모를 기록
-
-void BFS(int Start);
-void PrintShortestPath(int Start, int Dest);
-
-int main()
-{
-	PrintShortestPath(0,5);
-	return 0;
-}
-
-void BFS(int Start)
-{
-	queue<int> VertexQueue;
-	VertexQueue.push(Start);
-	Visited[Start] = true;
-	Distance[Start] = 0;
-	Parent[Start] = 0;
-
-	while (!VertexQueue.empty())
-	{
-		int Current = VertexQueue.front();
-		//cout << "방문 :" << Current << ", 부모누구?: "<< Parent[Current] << ", 거리몇?: "<< Distance[Current]<< endl;
-		VertexQueue.pop();
-
-		for (int Next = 0; Next < VertexCount; Next++)
-		{
-			if (Graph[Current][Next] == 0)
-				continue;
-			if (Visited[Next] == true)
-				continue;
-
-			VertexQueue.push(Next);
-			Visited[Next] = true;
-			Parent[Next] = Current;
-			Distance[Next] = Distance[Current] + 1;
-		}
-	}
-}
-
-void PrintShortestPath(int Start, int Dest)
-{
-	BFS(Start);
-	if (Visited[Dest] == false)
-	{
-		cout << "목적지까지 이동할 수 없습니다." << endl;
-		return;
-	}
-
-	vector<int> Path;
-
-	//스타트 지점까지 반복
-	int Temp = Dest;
-	while (Temp != Start)
-	{
-		Path.push_back(Temp);
-		Temp = Parent[Temp];
-	}
-	Path.push_back(Start);
-
-	//Path 순서 뒤집기
-	reverse(Path.begin(), Path.end());
-
-	//출력
-	for (int i = 0; i < Path.size(); i++)
-	{
-		cout << Path[i];
-		if (i < Path.size() - 1)
-		{
-			cout << "->";
-		}
-	};
-	cout << endl;
-
-	//reverse 사용하지 않고 출력하는 방법
-	//for (int i = Path.size() - 1; i >= 0; i--)
-	//{
-	//	cout << Path[i];
-	//	if (i > 0) 
-	//	{
-	//		cout << "->";
-	//	}
-	//}
-	//cout << endl;
-	//End_reverse 사용하지 않고 출력하는 방법
-}
+﻿//#include <iostream>
+//#include <queue>
+//#include <vector>
+//#include <algorithm>
+//
+////BFS
+////너비 우선 탐색
+////핵심 = 다음 방문할 정점을 미리 기억해 놓기
+////기억한 정점을 먼저 기억한 순서대로 방문 -> 선입선출
+//
+//using namespace std;
+//
+//const int VertexCount = 6;
+//
+//int Graph[VertexCount][VertexCount] =
+//{
+//	//0 1 2 3 4 5
+//	 {0,1,0,1,0,0},
+//	 {1,0,1,1,0,0},
+//	 {0,1,0,0,0,0},
+//	 {1,1,0,0,1,0},
+//	 {0,0,0,1,0,1},
+//	 {0,0,0,0,1,0}
+//};
+//
+//bool Visited[VertexCount] = { 0 };
+//
+//int Distance[VertexCount] = { 0 }; // 시작 지점으로 부터 떨어진 거리 기록
+//int Parent[VertexCount] = { 0 };   // 해당 정점의 부모를 기록
+//
+//void BFS(int Start);
+//void PrintShortestPath(int Start, int Dest);
+//
+//int main()
+//{
+//	PrintShortestPath(0,5);
+//	return 0;
+//}
+//
+//void BFS(int Start)
+//{
+//	queue<int> VertexQueue;
+//	VertexQueue.push(Start);
+//	Visited[Start] = true;
+//	Distance[Start] = 0;
+//	Parent[Start] = 0;
+//
+//	while (!VertexQueue.empty())
+//	{
+//		int Current = VertexQueue.front();
+//		//cout << "방문 :" << Current << ", 부모누구?: "<< Parent[Current] << ", 거리몇?: "<< Distance[Current]<< endl;
+//		VertexQueue.pop();
+//
+//		for (int Next = 0; Next < VertexCount; Next++)
+//		{
+//			if (Graph[Current][Next] == 0)
+//				continue;
+//			if (Visited[Next] == true)
+//				continue;
+//
+//			VertexQueue.push(Next);
+//			Visited[Next] = true;
+//			Parent[Next] = Current;
+//			Distance[Next] = Distance[Current] + 1;
+//		}
+//	}
+//}
+//
+//void PrintShortestPath(int Start, int Dest)
+//{
+//	BFS(Start);
+//	if (Visited[Dest] == false)
+//	{
+//		cout << "목적지까지 이동할 수 없습니다." << endl;
+//		return;
+//	}
+//
+//	vector<int> Path;
+//
+//	//스타트 지점까지 반복
+//	int Temp = Dest;
+//	while (Temp != Start)
+//	{
+//		Path.push_back(Temp);
+//		Temp = Parent[Temp];
+//	}
+//	Path.push_back(Start);
+//
+//	//Path 순서 뒤집기
+//	reverse(Path.begin(), Path.end());
+//
+//	//출력
+//	for (int i = 0; i < Path.size(); i++)
+//	{
+//		cout << Path[i];
+//		if (i < Path.size() - 1)
+//		{
+//			cout << "->";
+//		}
+//	};
+//	cout << endl;
+//
+//	//reverse 사용하지 않고 출력하는 방법
+//	//for (int i = Path.size() - 1; i >= 0; i--)
+//	//{
+//	//	cout << Path[i];
+//	//	if (i > 0) 
+//	//	{
+//	//		cout << "->";
+//	//	}
+//	//}
+//	//cout << endl;
+//	//End_reverse 사용하지 않고 출력하는 방법
+//}
---
+++ Tree.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+template<typename T> 
+class TreeNode
+{
+public:
+	TreeNode(const T& Value) :Data(Value)
+	{
+	}
+	~TreeNode()
+	{
+		for (TreeNode<T>* Child : Children)
+		{
+			delete Child;
+		}
+	}
+
+	T Data;
+	vector<TreeNode<T>*> Children;
+};
+
+TreeNode<string>* MakeTree();
+
+void PrintTree(TreeNode<string>* Root);
+void PrintTreeOrder(TreeNode<string>* Root);
+
+int GetHight(TreeNode<string>* Root);
+int GetHightTwoParm(TreeNode<string>* Root, int PreHight);
+
+
+
+int main()
+{
+    TreeNode<string>* Root = MakeTree();
+    PrintTree(Root); //깊이우선
+    PrintTreeOrder(Root); //너비우선  A B C 순으로
+    cout << MaxDepth << endl; //외국방식(?)
+    cout << GetHight(Root) << endl; //인자 1개
+    cout << GetHightTwoParm(Root, 0) << endl; //인자 2개
+    
+    delete Root;
+	return 0;
+}
+
+TreeNode<string>* MakeTree()
+{
+    TreeNode<string>* Root = new TreeNode<string>("A");
+    {
+        TreeNode<string>* NodeB = new TreeNode<string>("B");
+        {
+            TreeNode<string>* NodeD = new TreeNode<string>("D");
+            TreeNode<string>* NodeE = new TreeNode<string>("E");
+            {
+                TreeNode<string>* NodeI = new TreeNode<string>("I");
+                NodeE->Children.push_back(NodeI);
+            }
+            TreeNode<string>* NodeF = new TreeNode<string>("F");
+
+            NodeB->Children.push_back(NodeD);
+            NodeB->Children.push_back(NodeE);
+            NodeB->Children.push_back(NodeF);
+        }
+
+        TreeNode<string>* NodeC = new TreeNode<string>("C");
+        {
+            NodeC->Children.push_back(new TreeNode<string>("G"));
+            NodeC->Children.push_back(new TreeNode<string>("H"));
+        }
+
+        Root->Children.push_back(NodeB);
+        Root->Children.push_back(NodeC);
+    }
+
+    return Root;
+}
+
+int CurrnetDepth = 0;
+int MaxDepth = 0;
+void PrintTree(TreeNode<string>* Root)
+{
+    cout << Root->Data << endl;
+    if (Root->Children.empty())
+    {
+        if (CurrnetDepth > MaxDepth)
+        {
+            MaxDepth = CurrnetDepth;
+        }
+        CurrnetDepth = CurrnetDepth - 1;
+    }
+    for (TreeNode<string>* Child : Root->Children)
+    {
+        CurrnetDepth++;
+        PrintTree(Child);
+    }
+
+}
+
+int GetHightTwoParm(TreeNode<string>* Root, int PreHight)
+{
+    int Hight = PreHight;
+
+    for (TreeNode<string>* Child : Root->Children)
+    {
+        int NewHight = GetHightTwoParm(Child, PreHight + 1);
+        if (NewHight > Hight)
+        {
+            Hight = NewHight;
+        }
+    }
+
+    return Hight;
+}
+
+int GetHight(TreeNode<string>* Root)
+{
+    int Hight = 0;
+
+    for (TreeNode<string>* Child : Root->Children)
+    {
+        int ChildHight = GetHight(Child) + 1;
+        if (ChildHight > Hight)
+        {
+            Hight = ChildHight;
+        }
+    }
+
+    return Hight;
+}
+
+void PrintTreeOrder(TreeNode<string>* Root)
+{
+    queue<TreeNode<string>*> q;
+
+    q.push(Root);
+
+    while (!q.empty())
+    {
+        TreeNode<string>* Current = q.front();
+        q.pop();
+
+        cout << Current->Data << endl;
+
+        for (TreeNode<string>* Child : Current->Children)
+        {
+            q.push(Child);
+        }
+    }
+}
